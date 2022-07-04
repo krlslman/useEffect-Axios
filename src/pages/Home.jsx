f7ee17e3f2a79be5e8bd641933c9eb0f63d495f6
Selman Koral <selmankorall@gmail.com>
2022-07-04 13:17:03 +0300

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,65 @@
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+// aşağıyı ilgilendiren herşeyi bu parent ta yapabiliriz
+const Home = () => {
+  const [tutorials, setTutorials] = useState();
+
+  const url = `https://tutorials-api-cw.herokuapp.com/api/tutorials`;
+
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+  console.log(tutorials);
+
+  //POST
+  const addTutorial = async (tutorial) => {
+    try {
+      await axios.post(url, tutorial);
+
+      console.log("add");
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials(); // post tan sonra yeniden çekiyoruz güncel veriyi alıyoruz böyle state ya da props değiştirerek altta tutorials props unu değiştirmiş ve yeniden tetiklemiş oluyoruz
+  };
+
+  const deleteTutorial = async (id) => {
+    try {
+      await axios.delete(`${url}/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+
+  const editTutorial = async (id, title, desc) => {
+    const filtered = tutorials.filter((tutor) => tutor.id === id ).map(()=> ({title: title, description:desc}))  // return yerine paranteze aldık
+    console.log(filtered);
+    try {
+      await axios.put(`${url}/${id}`, filtered[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <AddTutorial addTutorial={addTutorial} />
+      <TutorialList tutorials={tutorials} deleteTutorial={deleteTutorial} editTutorial={editTutorial} />
+    </>
+  );
+};
+
+export default Home;
